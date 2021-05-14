cf331bb4b4ecea399ecf2c1bb8963a61d6831438
shameem2001 <toshameem2001@gmail.com>
2021-05-14 16:48:01 +0530

    start

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:glance/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   static const String id = "splash_screen";
@@ -10,8 +13,61 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 2),
+            () => Navigator.pushReplacementNamed(context, LoginScreen.id));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            child: Image.asset(
+                              'assets/glance.jpg',
+                              scale: 9,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 10.0),
+                          ),
+                          Text(
+                            'Glance',
+                            style: TextStyle(
+                                fontSize: 26,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w900,
+                                letterSpacing: 1.5),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          )),
+    );
   }
 }
