64fa86f2fb399b77cd537965068bc99925b6ae27
shameem2001 <toshameem2001@gmail.com>
2021-06-01 22:04:55 +0530

    DrawerBeta.dart changes

--- lib/components/DrawerBeta.dart
+++ lib/components/DrawerBeta.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:glance/utils/authentication.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -49,11 +50,25 @@ class DrawerBetaState extends State<DrawerBeta> {
               ),
               Divider(height: 10,thickness: 1,),
               ListTile(
-                leading: SvgPicture.asset('assets/svg/supportdrawer.svg',height: 22,),
+                leading: Icon(FontAwesomeIcons.userCircle, color: Colors.black,),
+                // SvgPicture.asset('assets/svg/supportdrawer.svg',height: 22,),
                 title: Text('Support',style: TextStyle(
                     fontFamily: "Montserrat",
                     fontSize: 15,
                     fontWeight: FontWeight.w800),),
+              ),
+              Divider(height: 10,thickness: 1,),
+              GestureDetector(
+                onTap: () {
+                //  navigate to settings screen.
+                },
+                child: ListTile(
+                  leading: Icon(Icons.settings,color: primaryColour,),
+                  title: Text('Settings',style: TextStyle(
+                      fontFamily: "Montserrat",
+                      fontSize: 15,
+                      fontWeight: FontWeight.w800),),
+                ),
               ),
               Divider(height: 10,thickness: 1,),
               GestureDetector(
@@ -69,13 +84,14 @@ class DrawerBetaState extends State<DrawerBeta> {
                   });
                 },
                 child: ListTile(
-                  leading: Icon(Icons.logout,color: primaryColour,),
+                  leading: Icon(FontAwesomeIcons.signOutAlt,color: primaryColour,),
                   title: Text('Logout',style: TextStyle(
                       fontFamily: "Montserrat",
                       fontSize: 15,
                       fontWeight: FontWeight.w800),),
                 ),
               ),
+              Divider(height: 10,thickness: 1,),
               SizedBox(height: 70,)
             ],
           ),
